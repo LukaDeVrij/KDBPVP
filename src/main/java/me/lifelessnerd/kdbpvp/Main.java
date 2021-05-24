bee53d48839a076b70d133a544a732e6c9a60ffd
PrinceMordred <jackrosenberg03@gmail.com>
2021-05-24 22:14:02 +0200

    22

--- src/main/java/me/lifelessnerd/kdbpvp/Main.java
+++ src/main/java/me/lifelessnerd/kdbpvp/Main.java
@@ -1,17 +1,49 @@
 package me.lifelessnerd.kdbpvp;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+public class Main extends JavaPlugin{
+    public void OnEnable() {
+        //on startup
+        //reloads
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void OnDisable() {
+        //on shutdown
+        //reloads
+
+    }
+    // / hello WHASSUP
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (label.equalsIgnoreCase("hello")) {
+            if (sender instanceof Player) {
+                //player
+                Player player = (Player) sender;
+                if (player.hasPermission("hello.use")) {
+                    player.sendMessage(ChatColor.LIGHT_PURPLE + "hey welcome cunt");
+                    //player.sendMessage(ChatColor.translateAlternateColorCodes('&', "have no fun"));
+                    return true;
+                }
+                else {
+                    player.sendMessage(ChatColor.DARK_RED + "REEEEEEEEEEEEEEEEEEEEEEEE BAD");
+                    return true;
+                }
+            }
+            else {
+                //console
+                sender.sendMessage("hey BITCH CONSOLE");
+                return true;
+
+            }
+        }
+
+
+
+
+        return false;
     }
 }
