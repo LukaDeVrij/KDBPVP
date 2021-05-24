fa9265d352485182e78d6d4aa177b7d97e3bfe72
LifelessNerd <35869162+LifelessNerd@users.noreply.github.com>
2021-05-24 23:28:34 +0200

    OnDeathEvent added, untested Ook is de potion gedefined, maar nog niet gegeven enkel debug tekst toegevoegd nadat de speler gedood is door instanceof player

--- src/main/java/me/lifelessnerd/kdbpvp/Main.java
+++ src/main/java/me/lifelessnerd/kdbpvp/Main.java
@@ -8,10 +8,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
     public void OnEnable() {
-        //on startup
-        //reloads
-    }
+        System.out.println("KDB PVP Plugin");
+        getServer().getPluginManager().registerEvents(new OnPlayerDeath(), this);
 
+    }
     public void OnDisable() {
         //on shutdown
         //reloads
@@ -19,10 +19,13 @@ public class Main extends JavaPlugin{
     }
     // / hello WHASSUP
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        System.out.println("1");
         if (label.equalsIgnoreCase("hello")) {
+            System.out.println("2");
             if (sender instanceof Player) {
                 //player
                 Player player = (Player) sender;
+                System.out.println("3");
                 if (player.hasPermission("hello.use")) {
                     player.sendMessage(ChatColor.LIGHT_PURPLE + "hey welcome cunt");
                     //player.sendMessage(ChatColor.translateAlternateColorCodes('&', "have no fun"));
@@ -40,8 +43,6 @@ public class Main extends JavaPlugin{
 
             }
         }
-
-
 
 
         return false;
--- src/main/java/me/lifelessnerd/kdbpvp/OnPlayerDeath.java
+++ src/main/java/me/lifelessnerd/kdbpvp/OnPlayerDeath.java
@@ -1,5 +1,34 @@
 package me.lifelessnerd.kdbpvp;
 
-public class OnPlayerDeath {
-    //HALLO JACK
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class OnPlayerDeath implements Listener {
+
+	@EventHandler
+	public void onDeath(PlayerDeathEvent event){
+	  Player player = event.getEntity();
+
+		// Potion
+		ItemStack itemToAdd = new ItemStack(Material.SPLASH_POTION, 1);
+		PotionMeta potionMeta = (PotionMeta) itemToAdd.getItemMeta();
+		PotionEffect potEffect = new PotionEffect(PotionEffectType.HEAL, 2, 2, true, true);
+        assert potionMeta != null; //idk wat dit is maar IntelliJ wilde het
+        potionMeta.addCustomEffect(potEffect, true);
+		itemToAdd.setItemMeta(potionMeta);
+
+	  if (player.getKiller() != null) {
+		  Player killer = player.getKiller();
+		  killer.sendMessage("You killed " + player.getName() + ", so you get some healing.");
+
+	  }
+	}
 }
