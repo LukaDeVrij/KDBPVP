f27495cfcadbdf163d1c4197351cec449f1164cd
LifelessNerd <35869162+LifelessNerd@users.noreply.github.com>
2021-05-28 21:17:07 +0200

    Killstreaks probeersel ge-add

--- src/main/java/me/lifelessnerd/kdbpvp/OnPlayerDeath.java
+++ src/main/java/me/lifelessnerd/kdbpvp/OnPlayerDeath.java
@@ -1,8 +1,8 @@
 package me.lifelessnerd.kdbpvp;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
 import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,11 +13,32 @@ import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.HashMap;
+
 public class OnPlayerDeath implements Listener {
+
+	HashMap<String, Integer> killStreaks = new HashMap<String, Integer>();
 
 	@EventHandler
 	public void onDeath(PlayerDeathEvent event){
 	  	Player player = event.getEntity();
+		Player killer = player.getKiller();
+
+		// Killstreaks probeersel
+		if (killer instanceof Player) {
+			if (killStreaks.containsKey(killer.getName())) {
+				killStreaks.replace(killer.getName(), killStreaks.get(killer.getName() + 1));
+				for(Player p : Bukkit.getOnlinePlayers()){
+					if(p.getWorld().getName().equals("pvp")){
+						p.sendMessage(Color.RED + killer.getName() + " heeft een killstreak van " + killStreaks.get(killer.getName()));
+					}
+				}
+			} else {
+				killStreaks.put(killer.getName(), 1);
+			}
+		}
+
+
 
 		// Potion
 		ItemStack potionItem = new ItemStack(Material.SPLASH_POTION, 1);
@@ -31,8 +52,6 @@ public class OnPlayerDeath implements Listener {
 		ItemStack arrowItem = new ItemStack(Material.ARROW, 2);
 
 		if (event.getEntity().getKiller() instanceof Player && event.getEntity().getWorld().getName().equalsIgnoreCase("pvp")) {
-
-			Player killer = event.getEntity().getKiller();
 
 			Inventory killerInventory = killer.getInventory();
 			killerInventory.addItem(potionItem);
